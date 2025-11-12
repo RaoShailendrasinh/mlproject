fe97c3da927267564aa9e6e976dff9a2813710d4
RaoShailendrasinh <raoshailendra18@gmail.com>
2025-11-12 18:27:48 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+
 from src.logger import logging
 from src.exception import CustomException
 
@@ -26,4 +28,27 @@ def save_object(file_path, obj):
 
     except Exception as e:
         logging.error(f"Error saving object at {file_path}: {e}")
+        raise CustomException(e, sys)
+    
+def evalute_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            # Train the model
+            model.fit(X_train, y_train)
+
+            # Predict on test data
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    except Exception as e:
         raise CustomException(e, sys)
